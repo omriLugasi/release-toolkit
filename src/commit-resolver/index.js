a010d8670b6d3fc185a8e2a8314ef29998ab66da
omri <omlrc1991@gmail.com>
2024-07-03 22:04:07 +0300

    feat(): add the option to work with ignore upgrade so you can use it for commit pattern and not for release

--- src/commit-resolver/index.js
+++ src/commit-resolver/index.js
@@ -4,13 +4,14 @@ const config = require('./../config')
 
 
 const versions = {
+  ignore: -1,
   build: 0,
   minor: 1,
   major: 2
 }
 
 const getNewSamver = (tag, weight) => {
-  const [build, minor, major] = tag.split('.').reverse()
+  const [major, minor, build] = tag.split('.')
   switch(weight) {
     case versions.build:
       return `${major}.${minor}.${parseInt(build, 10) + 1}`
@@ -41,7 +42,7 @@ module.exports = (tag, commits) => {
     let fitPatterns = false
     for (const { pattern, upgrade, title } of config.commitPatterns) {
       const regex = new RegExp(pattern)
-      if (regex.test(message)) {
+      if (regex.test(message) && versions[upgrade] !== versions.ignore) {
         affectedCommits.push({ ...commit, metadata: { ...commit.metadata, title: title } })
         upgrades.push(versions[upgrade])
         fitPatterns = true
