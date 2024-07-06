ed21f376d688dff23db6ab2b26914fb3fc75f1be
omri <omlrc1991@gmail.com>
2024-07-06 20:44:07 +0300

    feat(): fix the issue with the commit lint

---
+++ src/commit-lint/index.js
@@ -0,0 +1,31 @@
+const config = require('./../config')
+const fs = require('fs')
+const { promisify } = require('util')
+
+const readFileAsync = promisify(fs.readFile)
+
+
+const readFileContent = () => {
+  return readFileAsync(process.argv.slice(2)[0])
+}
+
+const init = async () => {
+  const commitMessage = (await readFileContent()).toString()
+
+  for (const { pattern } of config.commitPatterns) {
+    const regex = new RegExp(pattern)
+    if (regex.test(commitMessage)) {
+      process.exit(0)
+    }
+  }
+  console.log(`
+    Commit did not fit any of the provided patterns "${config.commitPatterns.map(item => item.pattern).join(' | ')}".
+    
+    Commit message:
+      ${commitMessage}
+  `)
+  process.exit(1)
+}
+
+
+init().then().catch(console.error)
